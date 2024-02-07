5131621cd89e499f6661cd8267a81d44c8d3cd31
WesleyR10 <wesleyribas2015@gmail.com>
2024-02-07 12:14:12 -0300

    Aula 2 -

---
+++ prisma/migrations/20240207132412_create_poll_options/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "Poll" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Poll_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PollOption" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "pollId" TEXT NOT NULL,
+
+    CONSTRAINT "PollOption_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "PollOption" ADD CONSTRAINT "PollOption_pollId_fkey" FOREIGN KEY ("pollId") REFERENCES "Poll"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
